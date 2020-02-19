8c290bdface6de64e1befe72d33e544105d1c719
rrprem <rheapremanand@gmail.com>
2020-02-19 14:08:21 -0500

    vers3

--- app/src/main/java/com/premanandrhea/lab3/MainActivity.java
+++ app/src/main/java/com/premanandrhea/lab3/MainActivity.java
@@ -2,36 +2,51 @@ package com.premanandrhea.lab3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView uno, dos, tres, quatro, center;
+    TextView uno, dos, tres, quatro;
     int c1, c2, c3, c4;
+    SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        center=findViewById(R.id.mid);
         uno=findViewById(R.id.one);
         dos=findViewById(R.id.two);
         tres=findViewById(R.id.three);
         quatro=findViewById(R.id.four);
 
-        c1=0;
-        c2=0;
-        c3=0;
-        c4=0;
+        c1 = sharedPreferences.getInt("1", 0);
+        c2 = sharedPreferences.getInt("2", 0);
+        c3 = sharedPreferences.getInt("3", 0);
+        c4 = sharedPreferences.getInt("4", 0);
+
+
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
 
         uno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 c1+=1;
                 uno.setText(c1+" ");
-                center.setText("the bottom right corner has been pressed");
+                editor.putInt("1", c1);
+                editor.apply();
+                Context context = getApplicationContext();
+                CharSequence text = "The bottom right corner has been clicked!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
         dos.setOnClickListener(new View.OnClickListener() {
@@ -39,7 +54,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 c2+=1;
                 dos.setText(c2+" ");
-                center.setText("the bottom left corner has been pressed");
+                editor.putInt("2", c2);
+                editor.apply();
+                Context context = getApplicationContext();
+                CharSequence text = "The bottom right corner has been clicked!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
         tres.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +69,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 c3+=1;
                 tres.setText(c3+" ");
-                center.setText("the top left corner has been pressed");
+                editor.putInt("3", c3);
+                editor.apply();
+                Context context = getApplicationContext();
+                CharSequence text = "The top left corner has been clicked!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
         quatro.setOnClickListener(new View.OnClickListener() {
@@ -55,9 +84,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 c4+=1;
                 quatro.setText(c4+" ");
-                center.setText("the top right corner has been pressed");
+                editor.putInt("4", c4);
+                editor.apply();
+                Context context = getApplicationContext();
+                CharSequence text = "The top right corner has been clicked!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
             }
         });
-
     }
 }
+
